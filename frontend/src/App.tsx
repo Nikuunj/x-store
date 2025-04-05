6064ca2b31c5b9375e132262fc3f19c9e1d8b748
Nikunj <mnikunj262@gmail.com>
2025-04-05 11:43:36 +0530

    [update]:themetogle button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,16 @@
-
-import { atomFamily, RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil';
-
-const elementPositionStateFamily = atomFamily({
-  key: 'ElementPosition',
-  default: ()=> [0],
-});
-
+import ThemeButton from "./components/ThemeButton"
 
 function App() {
+
+
   return (
     <>
-    <RecoilRoot>
-      <Render elementID={0} key={0}/>
-    </RecoilRoot>
+    <div className="dark:bg-black bg-zinc-100 transition-all duration-500 dark:text-zinc-100 h-screen">  
+      <ThemeButton />
+      hi there
+    </div>
     </>
   )
-}
-
-function Render({ elementID }) {
-  return <>
-  <br />
-    <ElementList elementID={elementID}/>
-    <Btn elementID={elementID}/>
-    <br />
-   </>
-}
-function ElementList({ elementID }) {
-  const position = useRecoilValue(elementPositionStateFamily(elementID));
-  return (
-    <div>
-      Element: {elementID}
-      <br />
-      Position: {position.toString()}
-      
-    </div>
-    )
-}
-
-function Btn( {elementID} ){
-  const setVal = useSetRecoilState(elementPositionStateFamily(elementID))
-  return  <>
-    <div onClick={() => setVal(pre => [...pre, pre[pre.length - 1] + 1])}>click me</div>
-    <div onClick={() => setVal([0])}>clear me</div>
-  </>
 }
 
 export default App
