0811e83ecb60e5c58a3791c2d5bfa1f8c4de73ff
Nikunj <mnikunj262@gmail.com>
2025-04-07 15:34:44 +0530

    create: star animation svg

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,10 +6,9 @@ import Product from "./pages/Product"
 function App() {
   
   return (
-    <div className=" cursor-default overflow-y-auto  font-mono dark:bg-black bg-zinc-100 transition-all duration-500 dark:text-zinc-100 h-screen">
-      <RecoilRoot>
-        <PlaceOrder />
-      </RecoilRoot>
+    <div className=" cursor-default overflow-y-auto  font-mono bg-[url(src/assets/star.svg)] bg-zinc-200 dark:bg-zinc-950 transition-all duration-500 dark:text-zinc-100 h-screen">
+      {/* <RecoilRoot>
+      </RecoilRoot> */}
     </div>
   )
 }
