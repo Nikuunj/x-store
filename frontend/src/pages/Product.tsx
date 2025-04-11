7efb9e8baa99546ca414fdacf5bc0307c3fe70b4
Nikunj <mnikunj262@gmail.com>
2025-04-11 15:47:15 +0530

    important: don't use chatGPT

--- frontend/src/pages/Product.tsx
+++ frontend/src/pages/Product.tsx
@@ -14,22 +14,21 @@ function Product() {
     return <>Error</>;
   }
 
-  
-    const renderProduct = val.contents.map((id: number | string) => (
-      <div
-        className="col-span-12 sm:col-span-6 md:col-span-6 lg:col-span-6 xl:col-span-4 flex items-center justify-center"
-        key={id}
-      >
-        <MiniProduct id={id.toString()} />
-        <ProductDetail id={id.toString()} />
-      </div>
-    ));
+  const renderProduct = val.contents.map((id: number | string) => (
+    <div
+      className="col-span-12 sm:col-span-6 md:col-span-6 lg:col-span-6 xl:col-span-4 flex items-center justify-center"
+      key={id}
+    >
+      <MiniProduct id={id.toString()} />
+      <ProductDetail id={id.toString()} />
+    </div>
+  ));
 
-    return (
-      <div className="grid grid-cols-12 md:px-24 sm:gap-x-10 sm:gap-y-12 gap-y-10 py-2 lg:mx-0">
-        {renderProduct}
-      </div>
-    );
+  return (
+    <div className="grid grid-cols-12 md:px-24 sm:gap-x-10 sm:gap-y-12 gap-y-10 py-2 lg:mx-0">
+      {renderProduct}
+    </div>
+  );
 
 }
 
