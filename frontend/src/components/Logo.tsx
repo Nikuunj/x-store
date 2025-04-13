7d36df12ac21134aca4ce8e6e25cda3294ad43ff
Nikunj <mnikunj262@gmail.com>
2025-04-14 02:19:25 +0530

    update: landing page

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,7 +1,7 @@
 
 function Logo() {
   return (
-    <div className={"bg-gradient-to-t ps-3 md:ps-0 from-zinc-800 from-20%  via-zinc-700 via-25% to-purple-600 to-65% inline-block text-transparent bg-clip-text text-3xl font-black"}>
+    <div className={"bg-gradient-to-t ps-3 md:ps-0 from-zinc-800 from-20%  via-zinc-800 via-25% to-purple-600 to-65% inline-block text-transparent bg-clip-text text-3xl font-black"}>
       X-Store</div>
   )
 }
